2b2c07f2724c2bfc401f3a6d0855631e871ba1d6
K7theCompSciMan <k7rang2018@gmail.com>
2023-10-02 18:01:33 -0400

    Changed Window

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame as p
 p.init()
+WIN = p.display.set_mode((800, 800))
+p.display.set_caption("8-Ball Pool")
+
 while True:
         for event in p.event.get():
-            if event.type == p.QUIT():
+            if event.type == p.QUIT:
                     p.quit()
